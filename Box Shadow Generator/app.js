ffe4302f535b943fc7756ec62bcfa385ec7dafd7
linnettjacobs <154928710+linnettjacobs@users.noreply.github.com>
2025-08-09 19:24:46 +0200

    Update app.js

--- Box Shadow Generator/app.js
+++ Box Shadow Generator/app.js
@@ -2,7 +2,12 @@ let sliders = document.querySelectorAll('input[type="range"]');
 let colors = document.querySelectorAll('input[type="color"]');
 let output = document.getElementById("css-code");
 let btnCopy = document.getElementById("copy");
+sliders.forEach((slider) => {
+  slider.addEventListener("input", createBox);
+});
 
+colors.forEach((color) => {
+  color.addEventListener("change", createBox);
 sliders.forEach((slider) => {
   slider.addEventListener("input", createBox);
 });
